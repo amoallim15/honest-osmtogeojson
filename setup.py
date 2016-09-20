ff5a57b357996b8b84b877c96181d742c1b1ce92
axj15 <axj.159@gmail.com>
2016-09-20 21:11:10 +0300

    an initial pip is ready.

---
+++ setup.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from os import path
+
+
+# __dirname = path.abspath(path.dirname(__file__))
+setup(
+    name= 'honest-osmtogeojson',
+    version= '1.0.0',
+    description= 'honest-osmtogeojson is a python module that converts OSM data represented in (XML format) into a GeoJSON data represented in (JSON), inspired by the JavaScript module osmtogeojson.',
+    url= 'https://github.com/AXJ15/honest-osmtogeojson',
+    author= 'Ali Moallim',
+    author_email= 'axj.159@gmail.com',
+    license= 'None',
+    keywords= 'osm geojson converter honest',
+    packages= ['hotgj'],
+    entry_points= { 'console_scripts': ['honest-osmtogeojson = hotgj:main'] },
+    classifiers= [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
